9701c1f255516128e4a863cd7954f7f55a02055f
soumaya <soumayabenhamadi19@gmail.com>
2025-05-22 11:20:45 +0200

    update route

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -7,6 +7,46 @@ export async function PATCH(
     request: NextRequest,
     { params }: { params: { userId: string } }
 ) {
-    console.log(await request.json());
-return NextResponse.json({ message: "User updated successfully" }, { status: 200 });
-}
+ try {
+      const session = await auth.api.getSession({ headers: await headers() });
+
+    if (!session?.user) {
+      return NextResponse.json(
+        { error: "Authentication required" },
+        { status: 401 },
+      );
+    } 
+    
+    const { userId } = await params;
+    if (session.user.id !== userId) {
+      return NextResponse.json(
+        { error: "You are not authorized to update this user" },
+        { status: 403 },
+      );
+    }
+    console.log("userId", userId);
+    console.log("session.user.id", session.user.id);
+    const body = await request.json();
+    const { name, email, image} = body;
+
+    const updatedUser = await prisma.user.update({
+        where: { id: userId },
+        data: {
+            name,
+            email,
+            image,
+        },
+        });
+
+    return NextResponse.json({
+        message: "User updated successfully",
+        user: updatedUser
+    })  
+ } catch{
+    return NextResponse.json(
+      { error: "Authentication required" },
+      { status: 401 },
+    );
+  }
+ }
+
